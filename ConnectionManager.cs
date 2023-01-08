5b7ea1bdc96a8ebae559b75b711e688f1bbd820d
mightymaggot55 <michaelraw@hotmail.co.uk>
2023-01-08 21:35:27 +0000

    08.01.23 changes - Website still not working due to certificate errors. Halfway through implementing ConnectionManager class.

---
+++ ConnectionManager.cs
@@ -0,0 +1,17 @@
+﻿namespace Artworks_ASP.NET
+{
+    public class ConnectionManager
+    {
+        private string UserID;
+        private string Password;
+        private string Database;
+        private bool TrustedConnection;
+
+        public string _UserID()
+        {
+            return UserID;
+        }
+
+
+    }
+}
