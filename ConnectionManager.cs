8f822e15da6ecf0649b833fcbe8bffb94bb928b1
mightymaggot55 <michaelraw@hotmail.co.uk>
2023-01-09 19:29:13 +0000

    Fixed Enquiries Page - Was due to the connection string needs another parameter

--- ConnectionManager.cs
+++ ConnectionManager.cs
@@ -1,17 +1,43 @@
 ﻿namespace Artworks_ASP.NET
 {
-    public class ConnectionManager
+    public static class ConnectionManager
     {
-        private string UserID;
-        private string Password;
-        private string Database;
-        private bool TrustedConnection;
+        private static string server = "DESKTOP-CBC069A";
+        private static string database = "Artworks";
+        private static string username = "DESKTOP-CBC069A\\Michael Raw";
+        private static string password = "YTanoi99";
+        private static string trustedConnection = "yes";
+        private static string connectionTimeout = "30";
+        private static string connectionString = string.Format("user id={0};password={1};server={2};Trusted_Connection={3};database={4};connection timeout={5};TrustServerCertificate=True",
+            Username, Password, Server, TrustedConnection, Database, ConnectionTimeout);
 
-        public string _UserID()
+        public static string ConnectionString
         {
-            return UserID;
+            get { return connectionString; }
         }
-
-
+        public static string Server
+        {
+            get { return server; }
+        }
+        public static string Database
+        {
+            get { return database; }
+        }
+        public static string Username
+        {
+            get { return username;}
+        }
+        public static string TrustedConnection
+        {
+            get { return trustedConnection; }
+        }
+        public static string Password
+        {
+            get { return password; }
+        }
+        public static string ConnectionTimeout
+        {
+            get { return connectionTimeout; }
+        }
     }
 }
